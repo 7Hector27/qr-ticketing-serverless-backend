7ef715bbb1c3cc675149179046c22c2f9bdf6465
Hector Carbajal <hector27c7@gmail.com>
2025-09-10 17:14:16 -0700

    initial commit

---
+++ handlers/getUsers.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { ddb } from "../utils/db";
+
+export const main: APIGatewayProxyHandlerV2 = async () => {
+  try {
+    const result = await ddb
+      .scan({ TableName: process.env.USERS_TABLE! })
+      .promise();
+    return { statusCode: 200, body: JSON.stringify(result.Items) };
+  } catch (err: any) {
+    console.error(err);
+    return { statusCode: 500, body: err.message };
+  }
+};
