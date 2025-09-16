85eea8e6ba16dbb9feccb7cf852dd861954906c9
Hector Carbajal <hector27c7@gmail.com>
2025-09-15 17:17:46 -0700

    added createEvent route

---
+++ handlers/events/createEvent.ts
@@ -0,0 +1,82 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { ddb } from "../../utils/db";
+import { v4 as uuid } from "uuid";
+import { verifyAuth } from "../../utils/auth";
+
+export const main: APIGatewayProxyHandlerV2 = async (event) => {
+  try {
+    //  Authenticate request
+    const auth = verifyAuth(event);
+
+    //  Authorize: only admin/staff can create events
+    if (auth.role !== "admin" && auth.role !== "staff") {
+      return {
+        statusCode: 403,
+        body: JSON.stringify({ message: "Forbidden" }),
+      };
+    }
+
+    const body = JSON.parse(event.body ?? "{}");
+    const {
+      title,
+      description,
+      date,
+      time,
+      location,
+      price,
+      quantity,
+      featured,
+    } = body;
+
+    // Event Validation
+    if (
+      !title ||
+      !description ||
+      !date ||
+      !time ||
+      !location ||
+      !price ||
+      !quantity
+    ) {
+      return {
+        statusCode: 400,
+        body: "Missing required fields",
+      };
+    }
+    const now = new Date().toISOString();
+    const eventId = uuid();
+
+    const newEvent = {
+      eventId,
+      title,
+      description,
+      date,
+      time,
+      location,
+      price: Number(price),
+      totalTickets: Number(quantity),
+      availableTickets: Number(quantity),
+      createdAt: now,
+      updatedAt: now,
+      featured: featured,
+    };
+
+    //createEvent
+    await ddb
+      .put({
+        TableName: process.env.EVENTS_TABLE!,
+        Item: newEvent,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        newEvent,
+      }),
+    };
+  } catch (err: any) {
+    console.error(err);
+    return { statusCode: 500, body: err.message };
+  }
+};
